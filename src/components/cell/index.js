e143df4f1f3270220254c3f10ab291a82e350475
于花花 <994763537@qq.com>
2017-03-24 14:30:30 +0800

    添加组件

--- src/components/cell/index.js
+++ src/components/cell/index.js
@@ -1,1 +1,12 @@
 import React from 'react'
+export default class Cell extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    }
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
--- src/components/chess/index.js
+++ src/components/chess/index.js
@@ -1,1 +1,12 @@
 import React from 'react'
+export default class Chess extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    }
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
